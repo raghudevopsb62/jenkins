560ae9d8f87c6a86df0abc5ee7172dd4cbc83e81
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-05 08:39:29 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -1,15 +1,52 @@
-freeStyleJob('example') {
+folder('Terraform') {
+  displayName('Terraform')
+  description('Terraform')
+}
+
+freeStyleJob('Terraform/VPC') {
   scm {
     git {
       remote {
         name('origin')
-        url('https://github.com/raghudevopsb62/ansible.git')
+        url('https://github.com/raghudevopsb62/terraform-vpc.git')
       }
     }
   }
 
   steps {
-    shell('ls -ltr')
+    shell('make dev')
+  }
+
+}
+
+freeStyleJob('Terraform/ALB') {
+  scm {
+    git {
+      remote {
+        name('origin')
+        url('https://github.com/raghudevopsb62/terraform-mutable-alb.git')
+      }
+    }
+  }
+
+  steps {
+    shell('make')
+  }
+
+}
+
+freeStyleJob('Terraform/DB') {
+  scm {
+    git {
+      remote {
+        name('origin')
+        url('https://github.com/raghudevopsb62/terraform-databases.git')
+      }
+    }
+  }
+
+  steps {
+    shell('make')
   }
 
 }

d8ec6b14d4167e14caf2c4be05ff10cdee5efbf0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-08 07:51:21 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -3,30 +3,47 @@ folder('Terraform') {
   description('Terraform')
 }
 
-def jobs= [
-    [name : "VPC", git : "terraform-vpc"],
-    [name : "DB", git : "terraform-databases" ]
-]
+//
+//jobs.each {
+//  def newmap = it;
+//  //println("    ${it.name}: ${it.git}");
+//  x = it.git
+//  freeStyleJob("Terraform/${it.name}") {
+//    scm {
+//      git {
+//        remote {
+//          name('origin')
+//          url("https://github.com/raghudevopsb62/${x}.git")
+//        }
+//        branches('*/main')
+//      }
+//    }
+//
+//    steps {
+//      shell('make')
+//    }
+//
+//  }
+//}
+//
 
-jobs.each {
-  def newmap = it;
-  //println("    ${it.name}: ${it.git}");
-  x = it.git
-  freeStyleJob("Terraform/${it.name}") {
-    scm {
-      git {
-        remote {
-          name('origin')
-          url("https://github.com/raghudevopsb62/${x}.git")
-        }
-        branches('*/main')
-      }
+
+//def jobs= [
+//    [name : "VPC", git : "terraform-vpc"],
+//    [name : "DB", git : "terraform-databases" ]
+//]
+//
+//
+//jobs.each {
+//  def newmap = it;
+//  x = it.git
+//
+//}
+
+pipelineJob('example') {
+  definition {
+    cpsScm {
+      github('jenkinsci/job-dsl-plugin')
     }
-
-    steps {
-      shell('make')
-    }
-
   }
 }
-

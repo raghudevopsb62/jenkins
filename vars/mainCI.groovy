aebfbabc34ea4facda93feb35f067de2ea6fa65a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-14 08:33:21 +0530

    Update jenkins/

---
+++ vars/mainCI.groovy
@@ -0,0 +1,69 @@
+def call() {
+  def skipRemainingStages = false
+
+  if (! env.COMPONENT) {
+    println "Component Missing"
+    System.exit(1)
+  }
+  NODE = null
+  node {
+
+    environment {
+      TOKEN = credentials('GITHUB_TOKEN')
+    }
+
+    def s = checkout scm
+    //common.checkRelease()
+    //common.createRelease(s.GIT_URL)
+
+    if(env.TYPE == "nodejs") {
+      println 'Yes NodeJS'
+    }
+
+//      stage('Download Dependencies') {
+//        when {
+//          expression {
+//            !skipRemainingStages
+//          }
+//        }
+//        steps {
+//          sh '''
+//          npm install
+//        '''
+//        }
+//      }
+//
+//
+//      stage('Make Artifacts') {
+//        when {
+//          expression {
+//            !skipRemainingStages
+//          }
+//        }
+//        steps {
+//          sh '''
+//          TAG=$(cat VERSION | grep "^#[0-9].[0-9].[0-9]" | head -1|sed -e "s|#|v|")
+//          echo $TAG >version
+//          zip -r ${COMPONENT}-${TAG}.zip node_modules server.js version
+//        '''
+//        }
+//      }
+//
+//      stage('Publish Artifacts to Nexus') {
+//        when {
+//          expression {
+//            !skipRemainingStages
+//          }
+//        }
+//        steps {
+//          sh '''
+//          TAG=$(cat VERSION | grep "^#[0-9].[0-9].[0-9]" | head -1|sed -e "s|#|v|")
+//          curl -f -v -u admin:admin123 --upload-file ${COMPONENT}-${TAG}.zip http://172.31.9.227:8081/repository/${COMPONENT}/${COMPONENT}-${TAG}.zip
+//        '''
+//        }
+//      }
+//
+//
+//
+  }
+}

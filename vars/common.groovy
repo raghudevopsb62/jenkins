16ffbc614b04edeae64d22673c57fc8256e2bba1
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-14 07:55:55 +0530

    Update jenkins/

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,22 @@
 def checkRelease() {
-  def statusCode = sh script:"git ls-remote --tags origin | grep \$(cat VERSION | grep '^#' | head -1| sed -e 's|#|v|')", returnStatus:true
-  if (statusCode == 0) {
-    println "VERSION mentioned in main branch has already been tagged"
-    skipRemainingStages = true
+  stage('Check Release') {
+    when {
+      expression {
+        GIT_BRANCH == "main"
+      }
+    }
+    steps {
+      script {
+        def statusCode = sh script:"git ls-remote --tags origin | grep \$(cat VERSION | grep '^#' | head -1| sed -e 's|#|v|')", returnStatus:true
+        if (statusCode == 0) {
+          println "VERSION mentioned in main branch has already been tagged"
+          skipRemainingStages = true
+        }
+      }
+    }
   }
+
+
 }
 
 def createRelease() {
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,102 +6,80 @@ def call() {
     System.exit(1)
   }
 
-  pipeline {
-
-    agent {
-      node {
-        label 'workstation'
-      }
-    }
+  node('workstation') {
 
     environment {
       TOKEN = credentials('GITHUB_TOKEN')
     }
 
-    stages {
-      node {
-        stage('Check Release') {
-          when {
-            expression {
-              GIT_BRANCH == "main"
-            }
-          }
-          steps {
-            script {
-              common.checkRelease()
-            }
-          }
-        }
-      }
+      common.checkRelease()
 
-
-      stage('Create Release') {
-        when {
-          allOf {
-            expression { GIT_BRANCH == "main" }
-            expression { !skipRemainingStages }
-          }
-
-        }
-        steps {
-          script {
-            common.createRelease()
-          }
-        }
-      }
-
-
-      stage('Download Dependencies') {
-        when {
-          expression {
-            !skipRemainingStages
-          }
-        }
-        steps {
-          sh '''
-          npm install
-        '''
-        }
-      }
-
-
-      stage('Make Artifacts') {
-        when {
-          expression {
-            !skipRemainingStages
-          }
-        }
-        steps {
-          sh '''
-          TAG=$(cat VERSION | grep "^#[0-9].[0-9].[0-9]" | head -1|sed -e "s|#|v|")
-          echo $TAG >version
-          zip -r ${COMPONENT}-${TAG}.zip node_modules server.js version
-        '''
-        }
-      }
-
-      stage('Publish Artifacts to Nexus') {
-        when {
-          expression {
-            !skipRemainingStages
-          }
-        }
-        steps {
-          sh '''
-          TAG=$(cat VERSION | grep "^#[0-9].[0-9].[0-9]" | head -1|sed -e "s|#|v|")
-          curl -f -v -u admin:admin123 --upload-file ${COMPONENT}-${TAG}.zip http://172.31.9.227:8081/repository/${COMPONENT}/${COMPONENT}-${TAG}.zip
-        '''
-        }
-      }
-
-
-    }
-
-    post {
-      always {
-        cleanWs()
-      }
-    }
+//      stage('Create Release') {
+//        when {
+//          allOf {
+//            expression { GIT_BRANCH == "main" }
+//            expression { !skipRemainingStages }
+//          }
+//
+//        }
+//        steps {
+//          script {
+//            common.createRelease()
+//          }
+//        }
+//      }
+//
+//
+//      stage('Download Dependencies') {
+//        when {
+//          expression {
+//            !skipRemainingStages
+//          }
+//        }
+//        steps {
+//          sh '''
+//          npm install
+//        '''
+//        }
+//      }
+//
+//
+//      stage('Make Artifacts') {
+//        when {
+//          expression {
+//            !skipRemainingStages
+//          }
+//        }
+//        steps {
+//          sh '''
+//          TAG=$(cat VERSION | grep "^#[0-9].[0-9].[0-9]" | head -1|sed -e "s|#|v|")
+//          echo $TAG >version
+//          zip -r ${COMPONENT}-${TAG}.zip node_modules server.js version
+//        '''
+//        }
+//      }
+//
+//      stage('Publish Artifacts to Nexus') {
+//        when {
+//          expression {
+//            !skipRemainingStages
+//          }
+//        }
+//        steps {
+//          sh '''
+//          TAG=$(cat VERSION | grep "^#[0-9].[0-9].[0-9]" | head -1|sed -e "s|#|v|")
+//          curl -f -v -u admin:admin123 --upload-file ${COMPONENT}-${TAG}.zip http://172.31.9.227:8081/repository/${COMPONENT}/${COMPONENT}-${TAG}.zip
+//        '''
+//        }
+//      }
+//
+//
+//
+//    post {
+//      always {
+//        cleanWs()
+//      }
+//    }
 
   }
 }

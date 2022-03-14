4711a04dbe328c3ad3a5717171b017616747c012
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-14 08:01:43 +0530

    Update jenkins/

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,6 +2,8 @@ def checkRelease() {
   stage('Check Release') {
     if (env.BRANCH_NAME == "main") {
       script {
+        sh 'pwd'
+        sh 'ls -l '
         def statusCode = sh script:"git ls-remote --tags origin | grep \$(cat VERSION | grep '^#' | head -1| sed -e 's|#|v|')", returnStatus:true
         if (statusCode == 0) {
           println "VERSION mentioned in main branch has already been tagged"

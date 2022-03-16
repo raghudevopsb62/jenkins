b7570de5a0c6e871a735442ed762bba13481ae28
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-16 08:25:31 +0530

    Update jenkins/

---
+++ vars/terraform.groovy
@@ -0,0 +1,50 @@
+def call() {
+  pipeline {
+
+    agent {
+      node { label 'workstation' }
+    }
+
+    parameters {
+      string(name: 'APPLY_ENV', defaultValue: 'dev', description: 'On which env you want to run?')
+    }
+
+    options {
+      ansiColor('xterm')
+    }
+
+    stages {
+
+      stage('Terraform Init') {
+        steps {
+          sh '''
+            cd mutable
+            terraform init -backend-config=env-${APPLY_ENV}/backend.tfvars
+          '''
+        }
+      }
+
+      stage('Terraform Plan') {
+        steps {
+          sh '''
+            cd mutable
+            terraform plan -var-file=env-${APPLY_ENV}/main.tfvars
+          '''
+        }
+      }
+
+      stage('Terraform Apply') {
+        steps {
+          sh '''
+            cd mutable
+            terraform apply -auto-approve -var-file=env-${APPLY_ENV}/main.tfvars
+          '''
+        }
+      }
+
+    }
+
+  }
+
+//
+}

822ec06bff4caa56bf0d5f7e7084365e3b37b296
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 08:59:37 +0530

    Update jenkins/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -19,7 +19,7 @@ def call() {
       stage('Terraform Init') {
         steps {
           sh '''
-            cd mutable
+            cd immutable
             terrafile -f env-${APPLY_ENV}/Terrafile
             export TF_VAR_APP_VERSION=`cat ../VERSION | grep ^# | head -1 | sed -e 's/#//'`
             terraform init -backend-config=env-${APPLY_ENV}/backend.tfvars
@@ -30,7 +30,7 @@ def call() {
       stage('Terraform Plan') {
         steps {
           sh '''
-            cd mutable
+            cd immutable
             export TF_VAR_APP_VERSION=`cat ../VERSION | grep ^# | head -1 | sed -e 's/#//'`
             terraform plan -var-file=env-${APPLY_ENV}/main.tfvars -var APP_VERSION=${APP_VERSION}
           '''
@@ -40,7 +40,7 @@ def call() {
       stage('Terraform Apply') {
         steps {
           sh '''
-            cd mutable
+            cd immutable
             export TF_VAR_APP_VERSION=`cat ../VERSION | grep ^# | head -1 | sed -e 's/#//'` 
             terraform apply -auto-approve -var-file=env-${APPLY_ENV}/main.tfvars -var APP_VERSION=${APP_VERSION} -parallelism=1
           '''

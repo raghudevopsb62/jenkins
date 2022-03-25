07d332323305384012081c8b23f67645ccf0f263
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-25 07:57:12 +0530

    Update jenkins/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -50,7 +50,7 @@ def call() {
       stage('Instance Refresh') {
         steps {
           sh '''
-            aws autoscaling start-instance-refresh --auto-scaling-group-name ${COMPONENT}-${APPLY_ENV} --preferences \'{"InstanceWarmup": 400, "MinHealthyPercentage": 50}\'
+            aws autoscaling start-instance-refresh --auto-scaling-group-name ${COMPONENT}-${APPLY_ENV} --preferences \'{"InstanceWarmup": 120, "MinHealthyPercentage": 50}\'
           '''
         }
       }
